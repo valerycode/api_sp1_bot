94425bed07c97ae9cde2f19199b125b231d16056
«Valeriya <lera200@yandex.ru>
2021-01-18 13:16:49 +0300

    Fixed errors

--- homework.py
+++ homework.py
@@ -1,10 +1,9 @@
+import logging
 import os
 import time
 
-import logging
 import requests
 import telegram
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,53 +20,40 @@ RESPONSE_ERROR = ('Ошибка в ответе сервера при выпол
                   ' со следующими параметрами:'
                   ' URL: {URL_API}, введенный токен: {token},'
                   ' параметры: {params}, значение {key}: {item}')
-STATUS_ERROR = 'Неизвестный статус работы "{homework_name}"'
-STATUS_MESSAGE = 'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+STATUS_ERROR = 'Неизвестный статус работы "{name}: {status}"'
+STATUS_MESSAGE = 'У вас проверили работу "{name}"!\n\n{status}'
 ERROR_MESSAGE = 'Бот столкнулся с ошибкой: {error}'
 SEND_MESSAGE = 'Бот успешно отправил сообщение в Телеграм-чат {message}'
 BOT_LAUNCH = 'Бот успешно запушен'
-HOMEWORK_STATUSES_DICT = {
+HOMEWORK_STATUSES = {
     'reviewing': 'Работа взята в ревью',
     'approved': 'Ревьюеру всё понравилось,'
                 ' можно приступать к следующему уроку.',
     'rejected': 'К сожалению в работе нашлись ошибки.'}
-logging.basicConfig(
-    level=logging.INFO,
-    filename='main.log',
-    format='%(asctime)s; %(levelname)s; %(name)s; %(message)s')
+logging.basicConfig(level=logging.INFO, filename=__file__ + '.log')
 logger = logging.getLogger('__name__')
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
+    name = homework['homework_name']
     status = homework['status']
-    if status not in HOMEWORK_STATUSES_DICT:
-        message = STATUS_ERROR.format(homework_name=homework_name)
-        logging.info(message)
-        return message
-    verdict = HOMEWORK_STATUSES_DICT.get(status)
-    return STATUS_MESSAGE.format(homework_name=homework_name, verdict=verdict)
+    if status not in HOMEWORK_STATUSES:
+        raise ValueError(STATUS_ERROR.format(name=name, status=status))
+    return STATUS_MESSAGE.format(name=name, status=HOMEWORK_STATUSES[status])
 
 
 def get_homework_statuses(current_timestamp):
     params = {'from_date': current_timestamp}
+    args = dict(url=URL_PRAKTIKUM, headers=HEADERS, params=params)
     try:
-        response = requests.get(URL_PRAKTIKUM, headers=HEADERS, params=params)
+        response = requests.get(**args)
     except requests.exceptions.RequestException as error:
-        raise ConnectionError(REQUEST_ERROR.format(
-            URL_API=URL_PRAKTIKUM,
-            token=PRAKTIKUM_TOKEN,
-            params=params,
-            error=error)
-        )
+        raise ConnectionError(REQUEST_ERROR.format(**args, error=error))
     homeworks = response.json()
-    error_keys = ['error', 'code']
-    for key in error_keys:
+    for key in ['error', 'code']:
         if key in homeworks:
-            raise ValueError(RESPONSE_ERROR.format(
-                URL_API=URL_PRAKTIKUM,
-                token=PRAKTIKUM_TOKEN,
-                params=params,
+            raise ConnectionError(RESPONSE_ERROR.format(
+                **args,
                 key=key,
                 item=homeworks[key])
             )
@@ -75,22 +61,17 @@ def get_homework_statuses(current_timestamp):
 
 
 def send_message(message, bot_client):
-    try:
-        sent_message = bot_client.send_message(CHAT_ID, message)
-    except Exception as error:
-        raise ValueError(ERROR_MESSAGE.format(error=error))
     logging.info(SEND_MESSAGE.format(message=message))
-    return sent_message
+    return bot_client.send_message(CHAT_ID, message)
 
 
 def main():
-
     bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
+    logging.debug(BOT_LAUNCH)
     current_timestamp = int(time.time())
 
     while True:
         try:
-            logging.debug(BOT_LAUNCH)
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(
@@ -101,10 +82,11 @@ def main():
             time.sleep(300)
 
         except Exception as error:
-            print(ERROR_MESSAGE.format(error=error))
-            logging.error(error, exc_info=True)
+            logging.error(ERROR_MESSAGE.format(error=error), exc_info=True)
             time.sleep(5)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s, %(levelname)s, %(name)s, %(message)s')
     main()

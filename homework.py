468bd67e464b231cd3e1b6ab3e1c825b2e8f5827
«Valeriya <lera200@yandex.ru>
2021-01-18 16:29:25 +0300

    Fixed errors 2

--- homework.py
+++ homework.py
@@ -14,37 +14,36 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 URL_PRAKTIKUM = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 REQUEST_ERROR = ('Сбой в работе сети. Произошла ошибка {error} при выполнении'
-                 ' запроса со следующими параметрами: URL: {URL_API},'
-                 ' TOKEN: {token}, параметры: {params}')
+                 ' запроса со следующими параметрами: URL: {url},'
+                 ' TOKEN: {headers}, параметры: {params}')
 RESPONSE_ERROR = ('Ошибка в ответе сервера при выполнени запроса'
                   ' со следующими параметрами:'
-                  ' URL: {URL_API}, введенный токен: {token},'
+                  ' URL: {url}, введенный токен: {headers},'
                   ' параметры: {params}, значение {key}: {item}')
 STATUS_ERROR = 'Неизвестный статус работы "{name}: {status}"'
-STATUS_MESSAGE = 'У вас проверили работу "{name}"!\n\n{status}'
+STATUS_MESSAGE = 'У вас проверили работу "{name}"!\n\n{verdict}'
 ERROR_MESSAGE = 'Бот столкнулся с ошибкой: {error}'
 SEND_MESSAGE = 'Бот успешно отправил сообщение в Телеграм-чат {message}'
 BOT_LAUNCH = 'Бот успешно запушен'
-HOMEWORK_STATUSES = {
+VERDICTS = {
     'reviewing': 'Работа взята в ревью',
     'approved': 'Ревьюеру всё понравилось,'
                 ' можно приступать к следующему уроку.',
     'rejected': 'К сожалению в работе нашлись ошибки.'}
-logging.basicConfig(level=logging.INFO, filename=__file__ + '.log')
-logger = logging.getLogger('__name__')
+logger = logging.getLogger(__name__)
 
 
 def parse_homework_status(homework):
     name = homework['homework_name']
     status = homework['status']
-    if status not in HOMEWORK_STATUSES:
+    if status not in VERDICTS:
         raise ValueError(STATUS_ERROR.format(name=name, status=status))
-    return STATUS_MESSAGE.format(name=name, status=HOMEWORK_STATUSES[status])
+    return STATUS_MESSAGE.format(name=name, verdict=VERDICTS[status])
 
 
 def get_homework_statuses(current_timestamp):
-    params = {'from_date': current_timestamp}
-    args = dict(url=URL_PRAKTIKUM, headers=HEADERS, params=params)
+    args = dict(url=URL_PRAKTIKUM, headers=HEADERS,
+                params={'from_date': current_timestamp})
     try:
         response = requests.get(**args)
     except requests.exceptions.RequestException as error:
@@ -52,7 +51,7 @@ def get_homework_statuses(current_timestamp):
     homeworks = response.json()
     for key in ['error', 'code']:
         if key in homeworks:
-            raise ConnectionError(RESPONSE_ERROR.format(
+            raise RuntimeError(RESPONSE_ERROR.format(
                 **args,
                 key=key,
                 item=homeworks[key])
@@ -61,7 +60,6 @@ def get_homework_statuses(current_timestamp):
 
 
 def send_message(message, bot_client):
-    logging.info(SEND_MESSAGE.format(message=message))
     return bot_client.send_message(CHAT_ID, message)
 
 
@@ -74,8 +72,10 @@ def main():
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(
-                    new_homework.get('homeworks')[0]), bot_client)
+                message = parse_homework_status(
+                    new_homework.get('homeworks')[0])
+                send_message(message, bot_client)
+                logging.info(SEND_MESSAGE.format(message=message))
             current_timestamp = new_homework.get(
                 'current_date',
                 current_timestamp)
@@ -88,5 +88,7 @@ def main():
 
 if __name__ == '__main__':
     logging.basicConfig(
+        level=logging.INFO,
+        filename=__file__ + '.log',
         format='%(asctime)s, %(levelname)s, %(name)s, %(message)s')
     main()
